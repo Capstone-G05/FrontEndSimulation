8739034fc05e2233890b6cdb2e16b46407629ea0
DavidXav <xavierd@myumanitoba.ca>
2025-01-27 13:16:28 -0600

    APICONTROLLER

---
+++ src/APIController.js
@@ -0,0 +1,8 @@
+
+export class APIController{
+    constructor(modelMovementLayer){
+        this.ModelMovementLayer = modelMovementLayer;
+    }
+
+    //some code here, not really sure how it works but we'll get there
+}
